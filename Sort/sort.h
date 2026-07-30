bf3796bfd8295adecb005beac3fbbb6a4b91fb6e
Rupayan Mandal <92633725+RupayanMandal@users.noreply.github.com>
2026-07-30 11:30:30 +0530

    Add files via upload

---
+++ Sort/sort.h
@@ -0,0 +1,19 @@
+#ifndef SORTING_H
+#define SORTING_H
+
+// Merge Sort
+void mergeSort(int arr[], int left, int right);
+//void merge(int arr[], int left, int mid, int right);
+
+// Quick Sort
+void quickSort(int arr[], int low, int high);
+//int partition(int arr[], int low, int high);
+
+// Heap Sort
+void heapSort(int arr[], int n);
+//void heapify(int arr[], int n, int i);
+
+// Utility
+//void swap(int *a, int *b);
+
+#endif
